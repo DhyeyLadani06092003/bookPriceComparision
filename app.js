ab565377cc6dbea65e2e8e8a9eb5b8de06e6669b
DhyeyLadani06092004 <dhyeyladani945@gmail.com>
2024-02-25 16:47:15 +0530

    .

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const mysql = require("mysql2");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const path = require("path");
-const cors = require("cors");
 const app = express();
 const port = 5000;
 
